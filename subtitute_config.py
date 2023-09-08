51ed10a7b2f6abda216cc4e2d27fc018f8051d00
Huy Trinh <huy.trinh@supercomp07a.r-ccs27.riken.jp>
2023-09-08 14:43:44 +0900

    Able to complete the idea

--- subtitute_config.py
+++ subtitute_config.py
@@ -8,10 +8,11 @@ with open(file_path, 'r') as file:
     content = file.read()
 
 # Find the specific elif block
-pattern = r'(\s*elif \[\[ "\$1" = \*"\*BBB"\*\ \]\]; then.*?)(\s*fi\s*)'
+pattern = r'(\s*elif \[\[ "\$1" = \*"BBB"\* \]\]; then.*?)(\s*fi\s*)'
 match = re.search(pattern, content, re.DOTALL)
 
 if match:
+    print ("Match")
     new_block = match.group(1).replace('BBB', 'CCC')
     content = content.replace(match.group(0), match.group(1) + '\n' + new_block + match.group(2))
 
